d0bcf6d2feed82a331b12ce35fcfc01360e23c9c
Mark Bodis <75426536+kozaabOi@users.noreply.github.com>
2023-11-08 11:19:11 +0100

    rickandmorty api + deploy

---
+++ script.js
@@ -0,0 +1,24 @@
+const rootElement = document.querySelector("#root");
+
+const fetchUrl = (url) => fetch(url).then((res) => res.json());
+
+const characterComponent = (characterData) => `
+  <div class="char">
+    <img src=${characterData.image}>
+    <h2>${characterData.name}</h2>
+    <h3>appears in: ${characterData.episode.length}</h3>
+  </div>
+`;
+
+const init = () => {
+  fetchUrl("https://rickandmortyapi.com/api/character").then((data) => {
+    const info = data.info;
+    const characters = data.results;
+
+    characters.forEach((character) =>
+      rootElement.insertAdjacentHTML("beforeend", characterComponent(character))
+    );
+  });
+};
+
+init();
